737b6e5a5d6a6de38055bdc6f9d3af7cb4dbe368
HB9HIL <fabian.berg@hb9hil.org>
2024-08-25 07:32:17 +0200

    prepared configuration modal

--- application/views/plugins/index.php
+++ application/views/plugins/index.php
@@ -47,7 +47,31 @@
                                     <td style="vertical-align: middle;"><?php echo $plugin->author; ?></td>
                                     <td style="vertical-align: middle;"><a href="<?php echo $plugin->uri; ?>" target="_blank"><?= __("Go to source..."); ?></a></td>
                                     <td style="vertical-align: middle;"><?php echo $plugin->status ? '<span class="badge bg-success">' . __("Enabled") . '</span>' : '<span class="badge bg-seconday">' . __("Disabled") . '</span>'; ?></td>
-                                    <td style="vertical-align: middle;"><button id="plugin_conf_<?php echo $plugin->plugin_id; ?>" class="editPlugin btn btn-primary btn-sm"><i class="fas fa-wrench"></i></button></td>
+                                    <td style="vertical-align: middle;">
+                                        <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#pluginModal_<?php echo $plugin->plugin_id; ?>"><i class="fas fa-wrench"></i></button>
+                                        <div class="modal fade bg-black bg-opacity-50" id="pluginModal_<?php echo $plugin->plugin_id; ?>" tabindex="-1" aria-labelledby="pluginLabel_<?php echo $plugin->plugin_id; ?>" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+                                            <div class="modal-dialog modal-dialog-centered modal-lg">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="pluginLabel_<?php echo $plugin->plugin_id; ?>"><?= sprintf(__("Configuration for %s"), "'" . $plugin->name . "'"); ?></h5>
+                                                    </div>
+                                                    <div class="modal-body" style="text-align: left !important;">
+                                                        <div class="mb-3">
+                                                            <p><?= __("Configure the plugin settings."); ?></p>
+                                                            <br>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <form action="<?php echo site_url('plugin/edit'); ?>" method="post" style="display:inline;">
+                                                                <input type="hidden" name="plugin_id" value="<?php echo $plugin->plugin_id; ?>">
+                                                                <button type="submit" class="btn btn-success"><?= __("Save") ?></i></button>
+                                                            </form>
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?= __("Cancel") ?></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
                                 </tr>
                             <?php } ?>
                         </tbody>
