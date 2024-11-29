0b80257219cb6796508dfddb1261545635003fa2
HB9HIL <fabian.berg@hb9hil.org>
2024-11-29 22:16:21 +0100

    adjustments

--- run_once.sh
+++ run_once.sh
@@ -1,12 +1,8 @@
 sudo apt update
-sudo apt install -y libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libx11-xcb1 libxcb-dri3-0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libasound2 libpangocairo-1.0-0 libcups2 libnss3
+sudo apt install -y libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libx11-xcb1 libxcb-dri3-0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libasound2 libpangocairo-1.0-0 libcups2 libnss3 xvfb
 docker compose -f cypress_testserver.yml up -d
 npx cypress run
 docker compose -f cypress_testserver.yml down
-docker volume rm cypress_db_data
-docker volume rm cypress_wavelog_data
-docker volume rm root_wavelog-config
-docker volume rm root_wavelog-dbdata
-docker volume rm root_wavelog-uploads
-docker volume rm root_wavelog-userdata
-docker rmi cypress_testing-web
+docker volume rm cypress-tests_db_data
+docker volume rm cypress-tests_wavelog_data
+docker rmi cypress-tests-web
