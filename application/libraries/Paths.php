9f0be163b4cee04a9c9ac852c14987a50520db9c
HB9HIL <fabian.berg@hb9hil.org>
2024-12-10 20:00:52 +0100

    debugging

--- application/libraries/Paths.php
+++ application/libraries/Paths.php
@@ -38,7 +38,7 @@ class Paths
         if (file_exists($fullpath)) {
             return base_url($filepath) . '?v=' . filemtime($fullpath);
         } else {
-            log_message('debug', 'File does not exist: ' . $fullpath);
+            log_message('debug', 'File does not exist: ' . $fullpath . '; Search in code for: ' . $filepath);
         }
         return base_url($filepath);
     }
